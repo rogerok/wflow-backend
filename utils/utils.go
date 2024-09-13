e881755116be821b4bfc37ea3abc3a4e0bb9d82d
vyacheslav <fedorik833@gmail.com>
2024-09-13 16:27:23 +0300

    first test prepared

--- utils/utils.go
+++ utils/utils.go
@@ -1,9 +1,19 @@
 package utils
 
 import (
+	"fmt"
+	"github.com/joho/godotenv"
 	"golang.org/x/crypto/bcrypt"
 )
 
 func HashPassword(password string) ([]byte, error) {
 	return bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+}
+
+func LoadEnv() {
+	err := godotenv.Load()
+	if err != nil {
+		fmt.Printf("Could not load environment %s", err.Error())
+		return
+	}
 }
